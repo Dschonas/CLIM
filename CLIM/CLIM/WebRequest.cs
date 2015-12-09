0a399b4f883741fdcbe03cad174e30759f495974
Stefan Matar <stefanmatar@gmail.com>
2015-12-09 12:21:21 +0100

    Added switch statement

--- CLIM/CLIM/WebRequest.cs
+++ CLIM/CLIM/WebRequest.cs
@@ -11,24 +11,29 @@ namespace CLIM
 
     class DataRequest
     {
-        public void Request(string artist)
+        public void Request(string searchTerm, string typeOfSearch)
         {
-            //request from the URL
-            WebRequest request = WebRequest.Create("https://itunes.apple.com/search?term=Adele");
-            //getting the response
-            WebResponse response = request.GetResponse();
-            //checking the status of the response
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            //returning the stream of the request
-            Stream dataStream = response.GetResponseStream();
-            //reading the dataStream
-            StreamReader reader = new StreamReader(dataStream);
-            string dataFromURL = reader.ReadToEnd();
-            //display the response data
-            Console.WriteLine(dataFromURL);
+            switch (typeOfSearch)
+            {
+                case "artist":
+                    //request from the URL
+                    WebRequest request = WebRequest.Create("https://itunes.apple.com/search?term=" + searchTerm);
+                    //getting the response
+                    WebResponse response = request.GetResponse();
+                    //checking the status of the response
+                    Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                    //returning the stream of the request
+                    Stream dataStream = response.GetResponseStream();
+                    //reading the dataStream
+                    StreamReader reader = new StreamReader(dataStream);
+                    string dataFromURL = reader.ReadToEnd();
+                    //display the response data
+                    Console.WriteLine(dataFromURL);
+                    break;
+                default:
+                    break;
+            }
 
-            Console.ReadKey();
-            Console.WriteLine("TEST");
             Console.ReadKey();
         }
 
