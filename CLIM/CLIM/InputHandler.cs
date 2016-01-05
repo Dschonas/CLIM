906f18a87905341cd03b7422abac86cf2319d94a
Jonas Neumair <jonasneumair@gmail.com>
2016-01-05 17:12:43 +0100

    small changes regarding redundant methods

--- CLIM/CLIM/InputHandler.cs
+++ CLIM/CLIM/InputHandler.cs
@@ -14,7 +14,7 @@ namespace CLIM
     class InputHandler
     {
 
-//DESCRIPTION
+        //DESCRIPTION
 
         //possible inputs
         //  - search
@@ -24,10 +24,10 @@ namespace CLIM
 
         //      - after you have searched, you can decide whether it is saved offline or not
 
-//PROPERTIES
+        //PROPERTIES
         public string SearchTerm { get; set; }
 
-//CONSTRUCTOR
+        //CONSTRUCTOR
         public InputHandler()
         {
             bool done = false;
@@ -77,9 +77,9 @@ namespace CLIM
                             select (string)p["artistId"];
                         ObjectDumper.Write(postTitles);
 
-                //Console.WriteLine(rssTitle);
+                        //Console.WriteLine(rssTitle);
 
-                Console.ReadKey();
+                        Console.ReadKey();
 
                         done = true;
 
@@ -96,7 +96,7 @@ namespace CLIM
 
 
 
-//DATA REQUEST SECTION
+        //DATA REQUEST SECTION
 
         //Requests the Json file with the URL
         public string JsonRequest(string searchTerm)
@@ -171,63 +171,12 @@ namespace CLIM
 
         public void QueryArtist()
         {
-            string json = JsonRequest(SearchTerm);
-            //string json = "{'resultCount':2,'results': [{'wrapperType':'track', 'kind':'podcast', 'collectionId':1016200886, 'trackId':1016200886, 'artistName':'Pietcast']}]";
-            JObject jo = JObject.Parse(json);
-
-            //string rrr = jo["resultCount"];
-
-            XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(json);
-            ObjectDumper.Write(doc);
-
-
-
+            
         }
 
-
-
-
-
-
-
-        //{
-        // "resultCount":2,
-        // "results": [
-        //{"wrapperType":"track", "kind":"podcast", "collectionId":1016200886, "trackId":1016200886, "artistName":"Pietcast", 
-        //    "collectionName":"Pietcast Fakten gibt es woanders", "trackName":"Pietcast Fakten gibt es woanders", "collectionCensoredName":"Pietcast Fakten gibt es woanders", 
-        //    "trackCensoredName":"Pietcast Fakten gibt es woanders", "collectionViewUrl":"https://itunes.apple.com/us/podcast/pietcast-fakten-gibt-es-woanders/id1016200886?mt=2&uo=4", 
-        //    "feedUrl":"http://www.pietcast.com/feed/m4a/", "trackViewUrl":"https://itunes.apple.com/us/podcast/pietcast-fakten-gibt-es-woanders/id1016200886?mt=2&uo=4", 
-        //    "artworkUrl30":"http://is2.mzstatic.com/image/thumb/Music5/v4/c2/dc/e9/c2dce919-e156-8ddc-f95f-b5a93a0b2829/source/30x30bb.jpg", 
-        //    "artworkUrl60":"http://is2.mzstatic.com/image/thumb/Music5/v4/c2/dc/e9/c2dce919-e156-8ddc-f95f-b5a93a0b2829/source/60x60bb.jpg", 
-        //    "artworkUrl100":"http://is2.mzstatic.com/image/thumb/Music5/v4/c2/dc/e9/c2dce919-e156-8ddc-f95f-b5a93a0b2829/source/100x100bb.jpg", "collectionPrice":0.00, 
-        //    "trackPrice":0.00, "trackRentalPrice":0, "collectionHdPrice":0, "trackHdPrice":0, "trackHdRentalPrice":0, "releaseDate":"2015-11-30T20:18:00Z", 
-        //    "collectionExplicitness":"notExplicit", "trackExplicitness":"notExplicit", "trackCount":21, "country":"USA", "currency":"USD", "primaryGenreName":"Philosophy", 
-        //    "radioStationUrl":"https://itunes.apple.com/station/idra.1016200886", "artworkUrl600":"http://is2.mzstatic.com/image/thumb/Music5/v4/c2/dc/e9/c2dce919-e156-8ddc-f95f-b5a93a0b2829/source/600x600bb.jpg", 
-        //    "genreIds":["1443", "26", "1324", "1318", "1450"], "genres":["Philosophy", "Podcasts", "Society & Culture", "Technology", "Podcasting"]
-        //    }
-        //}
-
-
-
-
         //TO XML SECTION
-        public void QueryMedia()
+        public void ConvertToXML()
         {
-            //--------------------------------------------------
-            string json = JsonRequest(SearchTerm);
-            //string json = "{'resultCount':2,'results': [{'wrapperType':'track', 'kind':'podcast', 'collectionId':1016200886, 'trackId':1016200886, 'artistName':'Pietcast']}]";
-            XmlDocument doc = JsonConvert.DeserializeXmlNode(json, "results");
-
-            Console.WriteLine();
-            //ObjectDumper.Write(doc);
-
-            StringWriter stringWriter = new StringWriter();
-            XmlTextWriter xmlTextWriter = new XmlTextWriter(stringWriter);
-
-            doc.WriteTo(xmlTextWriter);
-
-            Console.WriteLine(stringWriter.ToString());
-            Console.ReadKey();
 
         }
 
